7c59d2f984f443819c4cefeff40a0f9ec97662f0
Robertarezza <roberta.rezza@gmail.com>
2024-03-26 18:22:36 +0100

    add: Milstone 2

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 //test
 //alert("ciao")
 
-//bottone
+//bottone genera
 const sendBtnElem = document.getElementById("send-btn"); // object | nulla
 console.log(sendBtnElem);
 
+
+
 //evento al click del bottone
 sendBtnElem.addEventListener("click", function() {
+
+
 //raccolta dati
 const nameInputElem = document.getElementById("username"); // string
 console.log(nameInputElem);
@@ -21,10 +25,12 @@ const kmValue = parseInt(kmInputElem.value);  // number
 console.log(kmValue, typeof kmValue);
 
 const ageInputElem= document.getElementById("age"); // object | null
-console.log((ageInputElem));
+console.log(ageInputElem);
 
 const ageValue= parseInt(ageInputElem.value);  // number
 console.log(ageValue, typeof ageValue);
+
+if (isNaN(kmValue) === false && isNaN(ageValue) === false && kmValue > 0 && ageValue > 0) {
 
 //calcolo prezzo biglietto per km
 const ticketPrice = kmValue * 0.21; // number
@@ -34,8 +40,13 @@ let discount = 0;
 
 if (ageValue < 18) {
   discount = 20;
+  document.getElementById("ticket_price").innerHTML = "Biglietto scontato 20%";
 } else if (ageValue > 65) {
   discount = 40;
+  document.getElementById("ticket_price").innerHTML = "Biglietto scontato 40%";
+}else {
+    discount = 0;
+    document.getElementById("ticket_price").innerHTML = "Biglietto standard";
 }
 console.log("discount in %" , discount);
 
@@ -45,6 +56,25 @@ console.log("discount in euro", totalDiscount);
 const finalTicketPrice = ticketPrice - totalDiscount; // number
 console.log("Final Price", finalTicketPrice);
 
-document.getElementById("ticket").innerHTML = finalTicketPrice.toFixed(2);
+//genera numero carrozza
+const number=Math.floor(Math.random() * 11); //number
+console.log(number, typeof number);
 
+//stampa
+document.getElementById("carriage").innerHTML = number;
+document.getElementById("name").innerHTML = nameValue;
+document.getElementById("ticket").innerHTML = finalTicketPrice.toFixed(2);
+} else{
+    alert("dati errati")
+}
+
+})
+
+
+//bottone annulla
+const BtnElem = document.getElementById("btn"); // object | nulla
+console.log(BtnElem);
+
+BtnElem.addEventListener("click", function() {
+    window.location.reload()
 })
